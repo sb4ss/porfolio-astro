0e73fdeaa42248f552a6255d0a67656978fbe69f
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-03 20:31:37 -0500

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,3 +2,28 @@ document.querySelector(".btn-nav").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
 });
+
+document.querySelector(".nav-link").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("navActive");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
+
+document.querySelector(".nav-link2").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("navActive");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
+
+document.querySelector(".nav-link3").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("navActive");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
+
+document.querySelector(".nav-link4").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("navActive");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
+
+document.querySelector(".nav-link5").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.toggle("navActive");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
