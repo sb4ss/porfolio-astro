e624d24b198239d34e9f664aabe41c458b8a5099
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-03 20:56:34 -0500

    noscroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,29 @@ document.querySelector(".btn-nav").addEventListener("click", () => {
 document.querySelector(".nav-link").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("noscroll");
 });
 
 document.querySelector(".nav-link2").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("noscroll");
 });
 
 document.querySelector(".nav-link3").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("noscroll");
 });
 
 document.querySelector(".nav-link4").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("noscroll");
 });
 
 document.querySelector(".nav-link5").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("noscroll");
 });
