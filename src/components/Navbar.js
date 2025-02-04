373e00f06fa5fc28e07c9655268163d14b7fd413
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-04 00:17:22 -0500

    animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,12 @@ document.querySelector(".btn-nav").addEventListener("click", () => {
   document.querySelector(".body").classList.toggle("noscroll");
 });
 
+document.querySelector(".quitnav").addEventListener("click", () => {
+  document.querySelector(".navbar").classList.remove("navActive");
+  document.querySelector(".btn-nav").classList.remove("active");
+  document.querySelector(".body").classList.remove("noscroll");
+});
+
 document.querySelector(".nav-link").addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("navActive");
   document.querySelector(".btn-nav").classList.toggle("active");
