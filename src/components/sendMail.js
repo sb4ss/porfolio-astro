418774e9deef1f2be2c905c4e175c5ac3d6b5382
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-07 01:13:19 -0500

    astro email works

--- src/components/sendMail.js
+++ src/components/sendMail.js
@@ -1,10 +1,40 @@
 document
   .querySelector(".formulario")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
 
     let nameValue = document.getElementById("name").value;
     let emailValue = document.getElementById("email").value;
-    let info = document.getElementById("info").value;
-    let asunto = document.getElementById("asunto").value;
+    let infoValue = document.getElementById("info").value;
+    let asuntoValue = document.getElementById("asunto").value;
+
+    const data = {
+      name: nameValue,
+      from: emailValue,
+      message: infoValue,
+      subject: asuntoValue,
+    };
+
+    await sendinfotobackend(data);
   });
+
+async function sendinfotobackend(data) {
+  try {
+    const response = await fetch(" http://localhost:3000/api/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al enviar los datos");
+    }
+
+    const result = await response.json();
+    console.log("Respuesta del backend:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
