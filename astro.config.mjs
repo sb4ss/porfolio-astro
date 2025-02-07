3fb3bd1cdc70c4acfd72de6976269e9dad590aaa
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-06 21:49:04 -0500

    astro/vercel SSR adapter for resend

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: vercel()
 });
